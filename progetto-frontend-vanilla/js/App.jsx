2ba730c839f2f250e2fe9b7ed37759c532282c49
gabrielecallegari <g.cllegari@itsrizzoli.it>
2022-05-27 12:46:39 +0200

    feat: finally i hope

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import {Button} from "test-1-react/atoms/button/Button"
 const App = () => ( 
     <div>
-        <h1>Hello React</h1>
+        <h1>Hello Siuuuuuuuuuummm</h1>
+        <Button label={"Siuuuuuuuuuuuuuuummmmmmmmmmm"} />
     </div>
 );
 
