c981b4cd7c0e15c2a03f70118fd2cb8248ff1f99
gabrielecallegari <g.cllegari@itsrizzoli.it>
2022-06-06 12:56:15 +0200

    feat: added input

---
+++ design-system/react/src/atoms/input/Input.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Input = ({ onData, placeholder, type }) => {
+  function onInput(InputEvent){
+      console.log("input.onData ", InputEvent);
+      onData(InputEvent.target.value);
+  }
+  return (
+    <input
+      onInput={onInput}
+      placeholder={placeholder}
+      type={type} 
+      
+    >
+    </input>
+  );
+};
+
+Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  onData: PropTypes.func,
+  isValid: PropTypes.bool,
+  type: PropTypes.string,
+};
+
+Button.defaultProps = {
+  onData: undefined,
+  type: "text",
+  isValid: undefined,
+};
