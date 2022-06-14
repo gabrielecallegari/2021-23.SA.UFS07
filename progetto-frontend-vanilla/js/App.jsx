c4578a87f83503f58e79656540be34b34c2bafd4
gabrielecallegari <g.cllegari@itsrizzoli.it>
2022-06-14 11:05:11 +0200

    feat: added input in  app jsx

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {Button} from "test-1-callegari/atoms/button/Button"
+import {Input} from "test-1-callegari/atoms/input/Input"
 const App = () => ( 
     <div>
         <h1>Hello Siuuuuuuuuuummm</h1>
         <Button label={"Siuuuuuuuuuuuuuuummmmmmmmmmm"} />
+        <Input id="ciao" placeholder="mioplace"/>
     </div>
 );
 
